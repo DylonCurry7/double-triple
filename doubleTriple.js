a9396896fb6a95377b61b301248f8071373b17df
Ammaar <fakirammaar8@gmail.com>
2020-06-17 12:48:20 +0200

    created my first test.

---
+++ doubleTriple.js
@@ -0,0 +1,18 @@
+function doubleTriple(number) {
+    
+
+    if (number < 10) 
+    return number * 2; 
+
+    else if (number === 10 || number > 20)
+    return number * 3
+
+    else if (number < 20)
+    return 0;
+
+    else (number === -10,null,number)
+    return 0;
+
+   
+    
+}
---
+++ tests/doubletriple.test.js
@@ -0,0 +1,17 @@
+describe("The doubleTriple function", function(){
+
+it("doubleTriple(4) should return 8", function() {
+    assert.equal(true, true);
+});
+
+
+it("doubleTriple(12) should return 36", function() {
+   assert.equal(true, true); 
+});
+
+
+it("doubleTriple(21) should return 0", function() {
+assert.equal(true, true);
+});
+
+});
